002884aa6fe284d7f26051e06abb89a3b9f512ad
Crane-lupert <fawkes4700@mgmail.com>
2020-03-23 05:09:47 +0900

    0320 안현 퀴즈 제출

---
+++ 안현/quiz3_2.py
@@ -0,0 +1,26 @@
+'''
+2.
+첫 번째 숫자를 두 번째 숫자부터 마지막 숫자까지 차례대로 비교하여
+가장 작은 값을 찾아 첫 번째에 놓고,  두번째 숫자를 세 번째 숫자부터
+마지막 숫자까지 차례대로 비교하여그 중 가장 작은 값을 찾아
+두 번째 위치에 놓는 과정을 반복하며 정렬하는것을 선택정렬이라고 합니다.
+주어진 리스트를 선택정렬함수(select_sort)를 생성하여 오름차순으로 정렬하시오
+list=[6,2,3,7,8,10,21,1]
+
+<입력>
+print(select_sort(list))
+
+<출력>
+[1, 2, 3, 6, 7, 8, 10, 21]
+'''
+
+def select_sort(N):
+    for i in range(len(N)-1):
+        min=i
+        for j in range(i+1, len(N)):
+            if N[min] > N[j] : min = j
+        N[i], N[min]= N[min], N[i]
+    return N
+
+list=[6,2,3,7,8,10,21,1]
+print(select_sort(list))
